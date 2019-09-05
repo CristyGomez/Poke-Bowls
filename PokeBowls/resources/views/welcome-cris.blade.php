fa431af5519b39fb5fea5d806982e7b393e181b0
Cristina Gomez <crisgomezd@gmail.com>
2019-09-05 01:05:52 -0400

    Archivo POKE BOWLS

---
+++ PokeBowls/resources/views/welcome-cris.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>POKE BOWLS</title>
+
+        <link href="css/app.css" rel="stylesheet">
+
+    </head>
+    <body>
+        <div class="bg-fixed bg-cover min-h-screen" style="background-image: url(img/background.jpg)">
+            <div class="font-bold font-hairline text-6xl text-center">
+                <h1><span class="text-white"> POKE B</span><span class="text-red-500">O</span><span class="text-white">WLS</span></h1>
+                
+            </div>
+        </div>
+    </body>
+</html>
